5b25cb32105b49795d118a339d919a154768bde4
Tony <erhu1994@gmail.com>
2015-08-22 19:19:35 +0800

    加入显示天气信息功能。

--- src/com/coolweather/app/activity/ChooseAreaActivity.java
+++ src/com/coolweather/app/activity/ChooseAreaActivity.java
@@ -5,7 +5,10 @@ import java.util.List;
 
 import android.app.Activity;
 import android.app.ProgressDialog;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.text.TextUtils;
 import android.view.View;
 import android.view.Window;
@@ -16,6 +19,7 @@ import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.coolweather.app.R;
 import com.coolweather.app.model.City;
 import com.coolweather.app.model.CoolWeatherDB;
 import com.coolweather.app.model.County;
@@ -23,7 +27,6 @@ import com.coolweather.app.model.Province;
 import com.coolweather.app.util.HttpCallbackListener;
 import com.coolweather.app.util.HttpUtil;
 import com.coolweather.app.util.Utility;
-import com.coolweather.app.R;
 
 public class ChooseAreaActivity extends Activity {
 
@@ -65,6 +68,13 @@ public class ChooseAreaActivity extends Activity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
+		if(prefs.getBoolean("city_selected", false)) {
+			Intent intent = new Intent(this,WeatherActivity.class);
+			startActivity(intent);
+			finish();
+			return;
+		}
 		requestWindowFeature(Window.FEATURE_NO_TITLE);
 		setContentView(R.layout.choose_area);
 		listView = (ListView) findViewById(R.id.list_view);
@@ -83,6 +93,12 @@ public class ChooseAreaActivity extends Activity {
 					} else if (currentLevel == LEVEL_CITY) {
 						selectedCity = cityList.get(index);
 						queryCounties();
+					} else if(currentLevel == LEVEL_COUNTY) {
+						String countyCode = countyList.get(index).getCountyCode();
+						Intent intent = new Intent(ChooseAreaActivity.this,WeatherActivity.class);
+						intent.putExtra("county_code", countyCode);
+						startActivity(intent);
+						finish();
 					}
 			}
 		
